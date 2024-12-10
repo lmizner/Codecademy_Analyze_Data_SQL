e35791dfbe7bb1eb01e129a047447078dd7b3fd3
Lauren Mizner <74742038+lmizner@users.noreply.github.com>
2024-12-09 18:37:53 -0600

    Add files via upload

---
+++ Analyze_Real_Data/User_Churn/user_churn.sql
@@ -0,0 +1,295 @@
+-- We’ve imported 4 months of data for a company from when they began selling subscriptions. 
+-- This company has a minimum commitment of 1 month, so there are no cancellations in the first month.
+-- The subscriptions table contains:
+--     - id
+--     - subscription_start
+--     - subscription_end
+
+-- Use the methodology provided in the narrative to calculate the churn for January 2017.
+SELECT 1.0 * 
+(
+  SELECT COUNT(*)
+  FROM subscriptions
+  WHERE subscription_start < '2017-01-01'
+  AND (
+    subscription_end
+    BETWEEN '2017-01-01'
+    AND '2017-01-31'
+  )
+) / (
+  SELECT COUNT(*) 
+  FROM subscriptions 
+  WHERE subscription_start < '2017-01-01'
+  AND (
+    (subscription_end >= '2017-01-01')
+    OR (subscription_end IS NULL)
+  )
+) 
+AS result;
+
+
+
+-- Use the methodology provided in the narrative to calculate the churn for January 2017.
+-- The subscriptions table contains:
+--     - id
+--     - subscription_start
+--     - subscription_end
+WITH enrollments AS 
+(SELECT *
+FROM subscriptions
+WHERE subscription_start < '2017-01-01'
+AND (
+  (subscription_end >= '2017-01-01')
+  OR (subscription_end IS NULL)
+)),
+status AS 
+(SELECT 
+CASE
+  WHEN (subscription_end > '2017-01-31')
+    OR (subscription_end IS NULL) THEN 0
+  ELSE 1
+  END as is_canceled,
+CASE
+  WHEN (subscription_start < '2017-01-01')
+    AND (
+      (subscription_end >= '2017-01-01')
+        OR (subscription_end IS NULL)
+    ) THEN 1
+    ELSE 0
+  END as is_active
+FROM enrollments
+)
+SELECT 1.0 * SUM(is_canceled)/SUM(is_active) FROM status;
+
+
+
+-- We will be using the months as a temporary table (using WITH) in the churn calculation.
+-- Create the months temporary table using WITH and SELECT everything from it so that you can see the structure.
+-- We need a table for January, February, and March of 2017.
+WITH months as
+(SELECT
+  '2017-01-01' as first_day,
+  '2017-01-31' as last_day
+UNION
+SELECT
+  '2017-02-01' as first_day,
+  '2017-02-28' as last_day
+UNION
+SELECT
+  '2017-03-01' as first_day,
+  '2017-03-31' as last_day
+)
+SELECT *
+FROM months;
+
+
+
+-- Create a cross_join temporary table that is a CROSS JOIN of subscriptions and months.
+WITH months AS
+(SELECT
+  '2017-01-01' as first_day,
+  '2017-01-31' as last_day
+UNION
+SELECT
+  '2017-02-01' as first_day,
+  '2017-02-28' as last_day
+UNION
+SELECT
+  '2017-03-01' as first_day,
+  '2017-03-31' as last_day
+),
+cross_join AS
+(SELECT *
+FROM subscriptions
+CROSS JOIN months)
+SELECT *
+FROM cross_join
+LIMIT 100;
+
+
+
+-- Add a status temporary table. This table should have the following columns:
+--     - id - selected from the cross_join table
+--     - month - this is an alias of first_day from the cross_join table. We’re using the first day of the month to represent which month this data is for.
+--     - is_active - 0 or 1, derive this column using a CASE WHEN statement
+
+-- The is_active column should be 1 if the subscription_start is before the month’s first_day and if the subscription_end is either after the month’s first_day or is NULL.
+WITH months AS
+(SELECT
+  '2017-01-01' as first_day,
+  '2017-01-31' as last_day
+UNION
+SELECT
+  '2017-02-01' as first_day,
+  '2017-02-28' as last_day
+UNION
+SELECT
+  '2017-03-01' as first_day,
+  '2017-03-31' as last_day
+),
+cross_join AS
+(SELECT *
+FROM subscriptions
+CROSS JOIN months),
+status AS
+(SELECT id, first_day as month,
+CASE
+  WHEN (subscription_start < first_day)
+    AND (
+      subscription_end > first_day
+      OR subscription_end IS NULL
+    ) THEN 1
+  ELSE 0
+END as is_active
+FROM cross_join)
+SELECT *
+FROM status
+LIMIT 100;
+
+
+
+-- Add an is_canceled column to the status temporary table. 
+-- Ensure that it is equal to 1 in months containing the subscription_end and 0 otherwise.
+-- Derive this column using a CASE WHEN statement. 
+-- You can use the BETWEEN function to check if a date falls between two others.
+WITH months AS
+(SELECT
+  '2017-01-01' as first_day,
+  '2017-01-31' as last_day
+UNION
+SELECT
+  '2017-02-01' as first_day,
+  '2017-02-28' as last_day
+UNION
+SELECT
+  '2017-03-01' as first_day,
+  '2017-03-31' as last_day
+),
+cross_join AS
+(SELECT *
+FROM subscriptions
+CROSS JOIN months),
+status AS
+(SELECT id, first_day as month,
+CASE
+  WHEN (subscription_start < first_day)
+    AND (
+      subscription_end > first_day
+      OR subscription_end IS NULL
+    ) THEN 1
+  ELSE 0
+END as is_active,
+CASE 
+  WHEN subscription_end BETWEEN first_day AND last_day THEN 1
+  ELSE 0
+END as is_canceled
+FROM cross_join)
+SELECT *
+FROM status
+LIMIT 100;
+
+
+
+-- Add a status_aggregate temporary table. This table should have the following columns:
+--     - month - selected from the status table
+--     - active - the SUM() of active users for this month
+--     - canceled - the SUM() of canceled users for this month
+WITH months AS
+(SELECT
+  '2017-01-01' as first_day,
+  '2017-01-31' as last_day
+UNION
+SELECT
+  '2017-02-01' as first_day,
+  '2017-02-28' as last_day
+UNION
+SELECT
+  '2017-03-01' as first_day,
+  '2017-03-31' as last_day
+),
+cross_join AS
+(SELECT *
+FROM subscriptions
+CROSS JOIN months),
+status AS
+(SELECT id, first_day as month,
+CASE
+  WHEN (subscription_start < first_day)
+    AND (
+      subscription_end > first_day
+      OR subscription_end IS NULL
+    ) THEN 1
+  ELSE 0
+END as is_active,
+CASE 
+  WHEN subscription_end BETWEEN first_day AND last_day THEN 1
+  ELSE 0
+END as is_canceled
+FROM cross_join),
+status_aggregate AS
+(SELECT
+  month,
+  SUM(is_active) as active,
+  SUM(is_canceled) as canceled
+FROM status
+GROUP BY month)
+SELECT *
+FROM status_aggregate;
+
+
+
+-- Add a SELECT statement to calculate the churn rate. The result should contain two columns:
+--     - month - selected from status_aggregate
+--     - churn_rate - calculated from status_aggregate.canceled and status_aggregate.active.
+WITH months AS
+(SELECT
+  '2017-01-01' as first_day,
+  '2017-01-31' as last_day
+UNION
+SELECT
+  '2017-02-01' as first_day,
+  '2017-02-28' as last_day
+UNION
+SELECT
+  '2017-03-01' as first_day,
+  '2017-03-31' as last_day
+),
+cross_join AS
+(SELECT *
+FROM subscriptions
+CROSS JOIN months),
+status AS
+(SELECT id, first_day as month,
+CASE
+  WHEN (subscription_start < first_day)
+    AND (
+      subscription_end > first_day
+      OR subscription_end IS NULL
+    ) THEN 1
+  ELSE 0
+END as is_active,
+CASE 
+  WHEN subscription_end BETWEEN first_day AND last_day THEN 1
+  ELSE 0
+END as is_canceled
+FROM cross_join),
+status_aggregate AS
+(SELECT
+  month,
+  SUM(is_active) as active,
+  SUM(is_canceled) as canceled
+FROM status
+GROUP BY month)
+SELECT
+  month,
+  1.0 * canceled/active AS churn_rate
+FROM status_aggregate;
+
+
+
+
+
+
+
+
+
